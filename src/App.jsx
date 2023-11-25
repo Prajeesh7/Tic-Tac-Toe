3ebd7d3086ff4fbf0224de2b1b11c01d52679aae
Prajeesh <prajeeshrajan07@gmail.com>
2023-11-25 15:58:33 +0530

    Square Board set and all elements arranged

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import './App.css'
+import Square from './Components/Square';
+function Board() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <div className='board-row'>
+        <Square value={1}/>
+        <Square value={2} />
+        <Square value={3}/>
+      </div>
+      <div className='board-row'>
+        <Square value={4}/>
+        <Square value={5}/>
+        <Square value={6}/>
+      </div>
+      <div className='board-row'>
+        <Square value={7}/>
+        <Square value={8}/>
+        <Square value={9}/>
+      </div>
+
+    </>
   );
 }
 
-export default App;
+export default Board;
